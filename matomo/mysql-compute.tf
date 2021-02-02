ab3706bb952a874f6d93397fbff666d102acc9ac
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-02-02 17:17:07 +0100

    Further fixes for Matomo use-case

--- matomo/mysql-compute.tf
+++ matomo/mysql-compute.tf
@@ -1,14 +1,3 @@
-#module "mds-instance" {
-#  source         = "./modules/mds-instance"
-#  admin_password = var.admin_password
-#  admin_username = var.admin_username
-#  availability_domain  = var.availablity_domain_name
-#  configuration_id = data.oci_mysql_mysql_configurations.shape.configurations[0].id
-#  compartment_ocid = var.compartment_ocid
-#  subnet_id = oci_core_subnet.private.id
-#  display_name = "MySQLInstance"
-#}
-
 module "mysql-compute-instance" {
   source                = "./modules/mysql-compute-instance"
   admin_password        = var.admin_password
