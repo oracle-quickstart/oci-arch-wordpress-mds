575249ba9270ddee4cd27851ed1145bf486493b2
Jeevan Joseph <jeevan.g@gmail.com>
2021-03-26 10:56:01 -0700

    Updated TF version and pinned

--- micro-deployment/provider.tf
+++ micro-deployment/provider.tf
@@ -1,6 +1,21 @@
 # Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
+
+terraform {
+  required_providers {
+
+    oci = {
+      source = "hashicorp/oci"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+
+  }
+  required_version = ">= 0.14"
+}
+
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region = var.region
