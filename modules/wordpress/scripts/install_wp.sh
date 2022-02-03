64ae5419fdb6c9d7fdb95cbf975022ae9289436c
lfeldman <lukasz.feldman@oracle.com>
2022-02-03 13:08:11 +0100

    Fixed modules/wordpress/scripts/install_wp.sh

--- modules/wordpress/scripts/install_wp.sh
+++ modules/wordpress/scripts/install_wp.sh
@@ -6,9 +6,6 @@ cd ${wp_working_dir}/www/
 wget https://wordpress.org/wordpress-${wp_version}.tar.gz
 tar zxvf wordpress-${wp_version}.tar.gz
 rm -rf html/ wordpress-${wp_version}.tar.gz
-#wget https://wordpress.org/latest.tar.gz
-#tar zxvf latest.tar.gz
-#rm -rf html/ latest.tar.gz
 mv wordpress html
 chown apache. -R html
 sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 512M/g' /etc/php.ini
