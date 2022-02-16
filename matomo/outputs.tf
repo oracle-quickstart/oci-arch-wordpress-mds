94b7c92989eeae87b8bcb477e6acf8c381b2ee0e
Nuno Gonçalves <87083656+nugoncal@users.noreply.github.com>
2022-02-16 12:12:57 +0100

    Fixed the sensitive = true issue

--- matomo/outputs.tf
+++ matomo/outputs.tf
@@ -31,6 +31,7 @@ output "mysql_instance_ip" {
 
 output "generated_ssh_private_key" {
   value = tls_private_key.public_private_key_pair.private_key_pem
+  sensitive = true
 }
 
 output "matomo_username" {
