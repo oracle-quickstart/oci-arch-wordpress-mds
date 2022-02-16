50b8316b61d910ab27743d4378805257deb63630
lfeldman <lukasz.feldman@oracle.com>
2022-02-16 13:37:48 +0100

    Fixed network.tf and datasource.tf for matomo

--- matomo/network.tf
+++ matomo/network.tf
@@ -42,6 +42,7 @@ resource "oci_core_route_table" "private_route_table" {
   vcn_id         = oci_core_virtual_network.wpmdsvcn.id
   display_name   = "RouteTableForMySQLPrivate"
   route_rules {
+    destination_type  = "CIDR_BLOCK"
     destination       = "0.0.0.0/0"
     network_entity_id = oci_core_nat_gateway.nat_gateway.id
   }
